01f5426aa66548ac42b6634003ec563262c6610a
luisbryandr <luisbdominguezr@gmail.com>
2023-02-16 18:12:45 -0500

    populated dummy Array with 20 quotes, dynamically rendering 1 at a time through use of button, quote entering form started

---
+++ src/assets/featureIdeas.js
@@ -0,0 +1,18 @@
+/* Notes File to jot down ideas for features we want to include in this app
+
+  
+    - Enter Quotes - Using dummy data first
+    - Edit quotes
+    - Delete Quotes
+    - Create RESTful database to be able to handle the above data (CRUD)
+    - Styling sheet to match our design provided by Nelson
+    - add a link to wikipedia page of the author of the quote
+
+
+      **** Live stream builidng an app *** in future
+
+
+
+
+
+*/
