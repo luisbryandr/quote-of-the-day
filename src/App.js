edb69360212d507a9c24ae4274e7b201c1fb4dff
Nelson Maldonado <play2nino@gmail.com>
2023-02-16 16:07:27 -0800

    sunset bg added and positioned to cover

--- src/App.js
+++ src/App.js
@@ -1,46 +1,37 @@
-import './App.css';
-import { useState } from 'react';
-import quotes from './assets/dummyData';
-
-
-
+import "./App.css"
+import { useState } from "react"
+import quotes from "./assets/dummyData"
 
 function App() {
-  const [entry,setEntry] = useState('The quote of the day is: Click the button to find out, happy learning')
+  const [entry, setEntry] = useState(
+    "The quote of the day is: Click the button to find out, happy learning"
+  )
 
-  const generateQuote = () =>{
-  
-    const newQuote = Math.floor(Math.random()*quotes.length)
+  const generateQuote = () => {
+    const newQuote = Math.floor(Math.random() * quotes.length)
     console.log(newQuote)
     setEntry(`"${quotes[newQuote].quote}" -${quotes[newQuote].author} `)
   }
   return (
-    <div className="App">{}
-      <h1>{entry}
-       
-      </h1>
+    <div className="App">
+      {}
+      <h1>{entry}</h1>
       <button onClick={generateQuote}>Generate Quote</button>
 
-      <form >
+      <form>
         <label for="name">Enter your Favorite Quote Here:</label>
-        <input 
+        <input
           type="text"
           placeholder="Your Favorite Quote Here"
-          value=""    
-          name="quote"
-          />
-        <label for="author">Enter the Author</label>
-        <input 
-          type="text"
-          placeholder='Author Here'
           value=""
-          name="author"
-          />
-        <button>Enter</button>  
+          name="quote"
+        />
+        <label for="author">Enter the Author</label>
+        <input type="text" placeholder="Author Here" value="" name="author" />
+        <button>Enter</button>
       </form>
-      
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
