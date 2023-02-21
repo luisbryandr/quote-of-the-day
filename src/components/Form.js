062459eeb228382a233f2d9206adc8c291b4cb97
Nelson Maldonado <play2nino@gmail.com>
2023-02-21 14:13:07 -0800

    fixed button to say send instead of enter

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,50 +1,47 @@
-import React, { useState } from "react";
-import styled from 'styled-components';
-import Button from '@mui/material/Button'
-import SendIcon from '@mui/icons-material/Send'
+import React, { useState } from "react"
+import styled from "styled-components"
+import Button from "@mui/material/Button"
+import SendIcon from "@mui/icons-material/Send"
 
 const Form = () => {
+  const [values, setValues] = useState({ id: "", quote: "", auth: "" })
 
-    const [values, setValues]  = useState({id: "", quote: "", auth:"" })
+  const sendData = (event) => {
+    event.preventDefault()
+    console.log(event)
 
+    // const newFormData = {
+    //   paragraph: event.target.paragraph.value,
+    //   author: event.target.author.value
+    // }
 
-
-    const sendData =(event)=>{
-      event.preventDefault()
- console.log(event)
-   
-  // const newFormData = { 
-  //   paragraph: event.target.paragraph.value,
-  //   author: event.target.author.value
-  // }
-
-  // setValues([...values, newFormData])
-}
-
+    // setValues([...values, newFormData])
+  }
 
   return (
     <StyledForm>
-          <form onSubmit={sendData}>
-            <label>
-              <p className="para">Enter your Favorite Quote Here:</p>
-            </label>
+      <form onSubmit={sendData}>
+        <label>
+          <p className="para">Enter your Favorite Quote Here:</p>
+        </label>
 
-            <textarea name="paragraph" cols="30" rows="10"></textarea>
-            <br></br>
-            <label>
-              <p className="author" name="author">Enter the Author</p>
-            </label>
+        <textarea name="paragraph" cols="30" rows="10"></textarea>
+        <br></br>
+        <label>
+          <p className="author" name="author">
+            Enter the Author
+          </p>
+        </label>
 
-            <input type="text" placeholder="Author Here" name="author" />
-            <br></br>
-            <br></br>
-            <Button type="submit" variant="contained" endIcon={<SendIcon />}>
-              Enter
-            </Button>
-          </form>
-      </StyledForm>
+        <input type="text" placeholder="Author Here" name="author" />
+        <br></br>
+        <br></br>
+        <Button type="submit" variant="contained" endIcon={<SendIcon />}>
+          Send
+        </Button>
+      </form>
+    </StyledForm>
   )
-  
 }
 
 export default Form
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,26 +1,35 @@
 import React from "react"
 import { Link } from "react-router-dom"
-import styled from "styled-components";
-
-const StyledLink = styled(Link)`
-  text-decoration: none;
-  color: white;  
-`
-
+import styled from "styled-components"
 
 function NavBar() {
   return (
-
-      <div>
-        <nav>
-              <StyledLink exact to="/">
-                Home
-              </StyledLink>
-              <StyledLink to="/form" >Your quote</StyledLink>
-        </nav>
-      </div>
-    
+    <div>
+      <Nav>
+        <StyledLink exact to="/">
+          Home
+        </StyledLink>
+        <StyledLink to="/form">Your quote</StyledLink>
+      </Nav>
+    </div>
   )
 }
 
 export default NavBar
+
+const StyledLink = styled(Link)`
+  // text-decoration: none;
+  color: white;
+  // border: white 1px solid;
+  padding: 3px;
+  hover {
+    background-color: yellow;
+  }
+`
+const Nav = styled.nav`
+  display: flex;
+  justify-content: center;
+  width: 100%;
+  gap: 20px;
+  // border: red 1px solid;
+`
