a9260831e8dc1667dc3909084c68e7f31e9079f8
luisbryandr <luisbdominguezr@gmail.com>
2023-02-21 14:30:02 -0500

    refactored Home component

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 /***  Import React Tools Here ***/
-import { useState } from "react"
+
 import { BrowserRouter as Router, Switch, Route} from "react-router-dom"
 
 /*** Import Components Here ***/
@@ -9,40 +9,18 @@ import NavBar from "./components/NavBar"
 
 /*** Import Styles heres ***/
 import styled from "styled-components"
-import Button from "@mui/material/Button"
 import "./App.css"
 
-/*** Import Data Here***/
-import quotes from "./assets/dummyData"
-
 function App() {
-  const [entry, setEntry] = useState(
-    "The quote of the day is: Click the button to find out, happy learning"
-  )
-
-  console.log(entry)
-
-  const generateQuote = () => {
-    const newQuote = Math.floor(Math.random() * quotes.length)
-    console.log(newQuote)
-
-    setEntry(`"${quotes[newQuote].quote}" -${quotes[newQuote].author} `)
-  }
   return (
     <div>
       <MainDiv className="App">
        <Router>
         <NavBar/>
-        <Header>
-          <p className="entryone">{entry}</p>
-          <Button variant="contained" onClick={generateQuote}>
-            Generate Quote
-          </Button>
-        </Header>
-          <Switch>
-            <Route exact path='/' component={Home}/>
-            <Route path='/form' component={Form}/>
-          </Switch>
+        <Switch>
+          <Route exact path='/' component={Home}/>
+          <Route path='/form' component={Form}/>
+        </Switch>
         </Router>
       </MainDiv>
     </div>
@@ -64,13 +42,3 @@ const MainDiv = styled.div`
   margin: 0 auto;
 `
 
-const Header = styled.div`
-  display: flex;
-  flex-flow: column;
-  align-items: center;
-  justify-content: space-between;
-  // border: black solid 2px;
-  width: 90%;
-  height: 40vh;
-}
-  `
--- src/components/Home.js
+++ src/components/Home.js
@@ -1,10 +1,46 @@
+/*** State Management ***/
+import { useState } from "react";
 import React from "react";
+import styled from "styled-components";
+import Button from "@mui/material/Button"
+/*** Import Data Here***/
+import quotes from "../assets/dummyData";
 
 
-const Home =()=>{
+
+
+const Home =()=> {
+    const [entry, setEntry] = useState(
+        "The quote of the day is: Click the button to find out, happy learning"
+      )
+    
+      console.log(entry)
+    
+      const generateQuote = () => {
+        const newQuote = Math.floor(Math.random() * quotes.length)
+        console.log(newQuote)
+    
+        setEntry(`"${quotes[newQuote].quote}" -${quotes[newQuote].author} `)
+      }
     return (   
-        <div>This is a Home component</div>
-    )
-}
+        <Header>
+          <p className="entryone">{entry}</p>
+          <Button variant="contained" onClick={generateQuote}>
+            Generate Quote
+          </Button>
+        </Header>
+        )
+    };
 
-export default Home
+export default Home;
+
+const Header = styled.div`
+  display: flex;
+  flex-flow: column;
+  align-items: center;
+  justify-content: space-between;
+  // border: black solid 2px;
+  width: 90%;
+  height: 40vh;
+}
+  `
