69a8a9d31a3c130df361924b55611cb615d8b58a
Nelson Maldonado <play2nino@gmail.com>
2023-02-21 13:27:47 -0800

    changed the font of the main quotes to white for consistancy

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 /***  Import React Tools Here ***/
 
-import { BrowserRouter as Router, Switch, Route} from "react-router-dom"
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom"
 
 /*** Import Components Here ***/
 import Home from "./components/Home"
@@ -15,12 +15,12 @@ function App() {
   return (
     <div>
       <MainDiv className="App">
-       <Router>
-        <NavBar/>
-        <Switch>
-          <Route exact path='/' component={Home}/>
-          <Route path='/form' component={Form}/>
-        </Switch>
+        <Router>
+          <NavBar />
+          <Switch>
+            <Route exact path="/" component={Home} />
+            <Route path="/form" component={Form} />
+          </Switch>
         </Router>
       </MainDiv>
     </div>
@@ -35,10 +35,9 @@ const MainDiv = styled.div`
   align-items: center;
   flex-direction: column;
   // background-color: grey;
-  color: black;
+  color: white;
 
   width: 80%;
   height: 80vh;
   margin: 0 auto;
 `
-
